0dd75b03336d0872388c8c6e28bf53247b4c436c
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2023-06-30 17:46:23 +0530

    dCRT() vs spaCRT()

--- man/temp-codes/temp-loop.R
+++ man/temp-codes/temp-loop.R
@@ -1,21 +1,56 @@
 
+library(spacrt)
 library(magrittr)
 
-n <- 20; p <- 2; B <- 100; normalize <- FALSE; return_resamples <- FALSE
+n <- 50; p <- 3; B <- 2000; normalize <- FALSE; return_resamples <- FALSE
 X_on_Z_fam <- "binomial"
 Y_on_Z_fam <- "poisson"
 
-expit <- function(x) return(exp(x)/(1+exp(x)))
-results <- list()
+results.dCRT <- list()
+results.spaCRT <- list()
 
-for(k in 1:100){
-  data <- list(X = rbinom(n = n, size = 1, prob = 0.2),
-               Y = rpois(n = n, lambda = 1),
-               Z = matrix(rnorm(n = n*p, mean = 0, sd = 1), nrow = n, ncol = p))
+system.time({
+  for(k in 1:500){
+    set.seed(k)
 
-  results[[k]] <- dCRT(data, X_on_Z_fam, Y_on_Z_fam, B, normalize, return_resamples)
+    data <- list(X = rbinom(n = n, size = 1, prob = 0.2),
+                 Y = rpois(n = n, lambda = 1),
+                 Z = matrix(rnorm(n = n*p, mean = 0, sd = 1), nrow = n, ncol = p))
 
-  print(k)
-}
+    results.dCRT[[k]] <- spacrt::dCRT(data, X_on_Z_fam, Y_on_Z_fam, B,
+                                      normalize, return_resamples)
+    results.spaCRT[[k]] <- spacrt::spaCRT(data, X_on_Z_fam, Y_on_Z_fam, normalize)
 
-results %>% lapply(function(val) val$p_value) %>% unlist() %>% hist()
+    print(k)
+  }
+})
+
+df.dCRT <- data.frame(x = results.dCRT %>%
+                            lapply(function(val) val$p_value) %>% unlist())
+df.spaCRT <- data.frame(x = results.spaCRT %>%
+                              lapply(function(val) val$p_value) %>% unlist())
+
+(hist.dCRT <- df.dCRT %>%
+                ggplot(aes(x=x)) +
+                geom_histogram(bins = 10, fill="#69b3a2",
+                               color="#e9ecef", alpha=0.7) +
+                xlab('p-values') +
+                ylab('Count') +
+                ylim(c(0,70)) +
+                ggtitle("dCRT") +
+                theme_light() +
+                theme(plot.title = element_text(hjust = 0.5, face = 'bold')))
+
+(hist.spaCRT <- df.spaCRT %>%
+                  ggplot(aes(x=x)) +
+                  geom_histogram(bins = 10, fill="#69b3a2",
+                                 color="#e9ecef", alpha=0.7) +
+                  xlab('p-values') +
+                  ylab('Count') +
+                  ylim(c(0,70)) +
+                  ggtitle("spaCRT") +
+                  theme_light() +
+                  theme(plot.title = element_text(hjust = 0.5, face = 'bold')))
+
+gridExtra::grid.arrange(hist.dCRT, hist.spaCRT, ncol = 2)
+
