72cd8d7be2f1c7232baf705e09e68ec8f22eda34
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2023-07-05 11:42:39 +0530

    added helper functions for wcgf and its derivatives

--- R/method_helpers.R
+++ R/method_helpers.R
@@ -1,1 +1,126 @@
 # method helpers will go here (e.g. solving saddlepoint equations)
+
+#' \code{wcgf} is a function computing the cumulant generating function (CGF) of
+#' distributions, multiplied by a weight function, from the GLM family
+#'
+#' @param s The point where the CGF will be computed.
+#' @param P A vector containing the parameter values of the family of distributions.
+#' @param W A vector containing the weights.
+#' @param fam The GLM family which includes the distribution whose CGF is being
+#' evaluated (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
+#' @return CGF of the weighted distribution evaluated at \code{s}.
+#'
+#' @examples
+#' wcgf(s = 5, P = c(1,1,2), W = c(1,1,1), fam = 'poisson')
+#'
+#' @export
+wcgf <- function(s, P, W, fam){
+
+  if(fam == 'binomial') return(sum(log(exp(s*W)*P + 1 - P)))
+  if(fam == 'gaussian') return()
+  if(fam == 'Gamma') return()
+  if(fam == 'inverse.gaussian') return()
+  if(fam == 'poisson') return(sum(P*(exp(s*W) - 1)))
+  if(fam == 'quasi') return()
+  if(fam == 'quasibinomial') return()
+  if(fam == 'quasipoisson') return()
+}
+
+#' \code{d1.wcgf} is a function computing the derivative of the cumulant generating
+#' function (CGF) of distributions, multiplied by a weight function, from the GLM family
+#'
+#' @param s The point where the CGF will be computed.
+#' @param P A vector containing the parameter values of the family of distributions.
+#' @param W A vector containing the weights.
+#' @param fam The GLM family which includes the distribution whose CGF is being
+#' evaluated (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
+#' @return CGF of the weighted distribution evaluated at \code{s}.
+#'
+#' @examples
+#' d1.wcgf(s = 5, P = c(1,1,2), W = c(1,1,1), fam = 'poisson')
+#'
+#' @export
+d1.wcgf <- function(s, P, W, fam){
+
+  if(fam == 'binomial') return(sum((W*P*exp(s*W)) / (exp(s*W)*P + 1 - P)))
+  if(fam == 'gaussian') return()
+  if(fam == 'Gamma') return()
+  if(fam == 'inverse.gaussian') return()
+  if(fam == 'poisson') return(sum(P*exp(s*W)*W))
+  if(fam == 'quasi') return()
+  if(fam == 'quasibinomial') return()
+  if(fam == 'quasipoisson') return()
+
+
+}
+
+#' \code{d2.wcgf} is a function computing the hessian of the cumulant generating
+#' function (CGF) of distributions, multiplied by a weight function, from the GLM family
+#'
+#' @param s The point where the CGF will be computed.
+#' @param P A vector containing the parameter values of the family of distributions.
+#' @param W A vector containing the weights.
+#' @param fam The GLM family which includes the distribution whose CGF is being
+#' evaluated (values can be \code{gaussian}, \code{binomial}, \code{poisson}, etc).
+#' @return CGF of the weighted distribution evaluated at \code{s}.
+#'
+#' @examples
+#' d2.wcgf(s = 5, P = c(1,1,2), W = c(1,1,1), fam = 'poisson')
+#'
+#' @export
+d2.wcgf <- function(s, P, W, fam){
+
+  if(fam == 'binomial'){
+    Q <- 1 - P
+    return(sum((W^2*P*Q*exp(s*W)) / (exp(s*W)*P + Q)^2))
+  }
+
+  if(fam == 'gaussian'){
+    Q <- 1 - P
+    return()
+  }
+
+  if(fam == 'Gamma'){
+    Q <- 1 - P
+    return()
+  }
+
+  if(fam == 'inverse.gaussian'){
+    Q <- 1 - P
+    return()
+  }
+
+  if(fam == 'poisson'){
+    return(sum(P*exp(s*W)*W^2))
+  }
+
+  if(fam == 'quasi'){
+    Q <- 1 - P
+    return()
+  }
+
+  if(fam == 'quasibinomial'){
+    Q <- 1 - P
+    return()
+  }
+
+  if(fam == 'quasipoisson'){
+    Q <- 1 - P
+    return()
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
