d36e959682e858c96a4d35edc84472b7eea8352c
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2025-04-02 17:45:04 -0400

    suppressPackageStartupMessages --> suppressMessages

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 # for suppressing dependency startup messages
 .onLoad <- function(libname, pkgname) {
-  invisible(suppressPackageStartupMessages(
+  invisible(suppressMessages(
     sapply(c("MASS", "stats", "sceptre", "statmod"),
            requireNamespace, quietly = TRUE)
   ))
