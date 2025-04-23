61cf4fd088c0aa1ab5425aa92ca9b625da71e7ba
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2025-04-23 17:33:07 -0400

    removed spa_cdf call from spacrt

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,7 @@
              requireNamespace, quietly = TRUE)
    ))
 
+   # call estimate_theta() from sceptre
    if (requireNamespace("sceptre", quietly = TRUE)) {
       estimate_theta <- get("estimate_theta", envir = asNamespace("sceptre"))
       assign("estimate_theta", estimate_theta, envir = ns)
