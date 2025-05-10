83516a13dcd8f5a78fd642a5b1dbb3c239c13585
Jyotishka Ray Choudhury <jyotishkaraychoudhury@gmail.com>
2025-05-10 17:42:44 -0400

    added a proper test for checking if all the mthods are working correctly or not

--- tests/testthat/test-main-methods.R
+++ tests/testthat/test-main-methods.R
@@ -3,9 +3,10 @@ library(spacrtutils)
 n <- 80; p <- 2; iterations <- 100
 
 X_on_Z_fam <- "binomial"; Y_on_Z_fam <- "poisson"
-fitting_X_on_Z <- 'rf'; fitting_Y_on_Z <- 'glm'
+fitting_X_on_Z <- 'random_forest'; fitting_Y_on_Z <- 'glm'
 
-results_GCM <- results_dCRT <- results_spaCRT <- list()
+results_GCM <- results_dCRT <- results_spaCRT <- results_score <- list()
+pb <- txtProgressBar(min = 0, max = n, style = 3)
 
 for(i in 1:iterations){
    set.seed(i)
@@ -29,6 +30,11 @@ for(i in 1:iterations){
       spaCRT_internal(data, X_on_Z_fam, Y_on_Z_fam,
                       fitting_X_on_Z = fitting_X_on_Z,
                       fitting_Y_on_Z = fitting_Y_on_Z)
+
+   results_score[[i]] <- score.test(data, X_on_Z_fam, Y_on_Z_fam)
+
+   Sys.sleep(0.005)
+   setTxtProgressBar(pb, i)
 }
 
 df.pvals <- data.frame(
@@ -39,21 +45,36 @@ df.pvals <- data.frame(
                results_spaCRT |> lapply(function(lst) lst$p.left |> unname()) |> unlist())
 )
 
-df.pvals |>
-   dplyr::filter(method == 'GCM' & p_value <= 0.05) |>
-   dplyr::summarize(n = dplyr::n()) |>
-   dplyr::pull(n) / iterations
+res.GCM <- df.pvals |>
+               dplyr::filter(method == 'GCM' & p_value <= 0.05) |>
+               dplyr::summarize(n = dplyr::n()) |>
+               dplyr::pull(n) / iterations
 
-df.pvals |>
-   dplyr::filter(method == 'dCRT' & p_value <= 0.05) |>
-   dplyr::summarize(n = dplyr::n()) |>
-   dplyr::pull(n) / iterations
+res.dCRT <- df.pvals |>
+               dplyr::filter(method == 'dCRT' & p_value <= 0.05) |>
+               dplyr::summarize(n = dplyr::n()) |>
+               dplyr::pull(n) / iterations
 
-df.pvals |>
-   dplyr::filter(method == 'spaCRT' & p_value <= 0.05) |>
-   dplyr::summarize(n = dplyr::n()) |>
-   dplyr::pull(n) / iterations
+res.spaCRT <- df.pvals |>
+                  dplyr::filter(method == 'spaCRT' & p_value <= 0.05) |>
+                  dplyr::summarize(n = dplyr::n()) |>
+                  dplyr::pull(n) / iterations
 
+res.score <- results_score |>
+                  lapply(function(elem) elem$p.left) |>
+                  unlist() |>
+                  {\(x) x[x <= 0.05]}() %>%
+                  mean() |>
+                  round(3)
+
+writeLines("")
+
+test_that("Hello",{
+   expect_equal(res.GCM, 0.08)
+   expect_equal(res.dCRT, 0.02)
+   expect_equal(res.spaCRT, 0.02)
+   expect_equal(res.score, 0.023)
+})
 
 
 
